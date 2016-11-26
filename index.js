6315b8c9c27707b35984fef651683098df85382d
JayWizz <jakobdissingvang@gmail.com>
2016-11-26 21:34:24 +0100

    dev

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ var pollingtoevent = require('polling-to-event');
 	    // url info
 		this.ip = config["ip"];
 		this.port = config["port"];
-		this.play_url = this.ip + ":" + this.port + "/play";        //config["on_url"];
+		this.play_url = config["on_url"];//this.ip + ":" + this.port + "/play";        
         this.play_body                = config["play_body"];
 
-        this.stop_url = this.ip + ":" + this.port + "/pause";        //config["off_url"];
+        this.stop_url = config["off_url"]; //this.ip + ":" + this.port + "/pause";        
 		this.stop_body               = config["stop_body"];
 
 		this.status_url = config["status_url"];
